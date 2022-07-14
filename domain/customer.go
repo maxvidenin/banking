3971e1f5e3868d57246294fdfcc055d74cde198d
Maksym Vidienin <videnin10@gmail.com>
2022-07-14 13:42:16 +0200

    add dto for customer domain

--- domain/customer.go
+++ domain/customer.go
@@ -1,17 +1,39 @@
 package domain
 
-import "github.com/maxvidenin/banking/errs"
+import (
+	"github.com/maxvidenin/banking/dto"
+	"github.com/maxvidenin/banking/errs"
+)
 
 type Customer struct {
-	Id          int64  `json:"id" db:"customer_id"`
-	Name        string `json:"name"` 
-	City        string `json:"city"` 
-	Zipcode     string `json:"zipcode"`
-	DateOfBirth string `json:"date_of_birth" db:"date_of_birth"`
-	Status      string `json:"status"`
+	Id          int64 `db:"customer_id"`
+	Name        string
+	City        string
+	Zipcode     string
+	DateOfBirth string `db:"date_of_birth"`
+	Status      string
 }
 
 type CustomerReporsitory interface {
 	FindAll(string) ([]Customer, *errs.AppError)
 	ById(string) (*Customer, *errs.AppError)
+}
+
+func (c Customer) statusAsText() string {
+	statusAsText := "active"
+	if c.Status == "0" {
+		statusAsText = "inactive"
+	}
+	return statusAsText
+}
+
+func (c Customer) ToDto() dto.CustomerResponse {
+	return dto.CustomerResponse{
+		Id:          c.Id,
+		Name:        c.Name,
+		City:        c.City,
+		Zipcode:     c.Zipcode,
+		DateOfBirth: c.DateOfBirth,
+		Status:      c.statusAsText(),
+	}
 }
